3c55982a97baeedaa6573d653361401510d0cefc
Teddy Amkie <teddyamkie@gmail.com>
2019-12-07 21:36:12 -0800

    Updated small things.

--- js/main.js
+++ js/main.js
@@ -2,220 +2,222 @@
 // We're going to create a (class) and populate it with methods of all the page functionality.
 
 // TODO: Turn this into an actual class.
-(function (window) {
+// (function (window) {
 
-    let Blocker = {
-        // Initalizers
-        isCurrencyPage: false,
-        usersBlocked: [],
-        reviewList: {},
-        init: function () {
-            this.checkPage()
-            this.getChromeStorage();
-            this.allWhispers();
-        },
-        checkPage: function () {
-            let host = window.location.host;
-            this.isCurrencyPage = host.indexOf('currency') > -1
-        },
-        // Get data from chrome storage and update page accordingly.
-        getChromeStorage: function () {
-            // We want to store the Blocker class as this so we don't end up calling methods on the window.
-            let self = this;
-            // Get data from chrome storage
-            chrome.storage.sync.get(['review-list'], function (reviewList) {
-                let results = reviewList['review-list'] ? reviewList['review-list'] : {};
-                // console.log("Storage succesfully receieved: ", results);
-                self.reviewList = results;
+let Blocker = {
+    // Initalizers
+    isCurrencyPage: false,
+    usersBlocked: [],
+    reviewList: {},
+    init: function () {
+        this.checkPage()
+        this.getChromeStorage();
+        this.allWhispers();
+    },
+    checkPage: function () {
+        let host = window.location.host;
+        this.isCurrencyPage = host.indexOf('currency') > -1
+    },
+    // Get data from chrome storage and update page accordingly.
+    getChromeStorage: function () {
+        // We want to store the Blocker class as this so we don't end up calling methods on the window.
+        let self = this;
+        // Get data from chrome storage
+        chrome.storage.sync.get(['review-list'], function (reviewList) {
+            let results = reviewList['review-list'] ? reviewList['review-list'] : {};
+            // console.log("Storage succesfully receieved: ", results);
+            self.reviewList = results;
 
-                // Modify page based on storage data
-                for (let user in self.reviewList) {
-                    if (self.reviewList[user].score > 0) {
-                        self.upvoteUserUI(user);
-                    }
-                    if (self.reviewList[user].score < 0) {
-                        self.downvoteUserUI(user);
-                    }
-                    if (self.reviewList[user].hidden) {
-                        self.hideUserUI(user);
-                    }
+            // Modify page based on storage data
+            for (let user in self.reviewList) {
+                if (self.reviewList[user].score > 0) {
+                    self.upvoteUserUI(user);
                 }
-            });
-
-
-            // chrome.storage.sync.get(['blocked-sellers'], function (blockedSellers) {
-            //     let results = blockedSellers['blocked-sellers'] ? blockedSellers['blocked-sellers'] : [];
-            //     self.usersBlocked = results;
-
-            //     _.each(self.usersBlocked, function (seller) {
-            //         self.hideUser(seller);
-            //     });
-            // });
-        },
-
-        // Set-up buttons:
-        allWhispers: function () {
-            let self = this;
-            let sellers = this.isCurrencyPage ? $('.displayoffer-bottom span.right a') : $('.whisper-btn');
-
-            _.each(sellers, function (seller) {
-                self.appendButtons(seller);
-            });
-        },
-        appendButtons: function (position) {
-            // block button
-            let hide = '<span class="block-seller-wrapper"><a class="hide-seller" href="#">Hide User</a></span>'
-
-            // upvote button
-            let upvote = '<span class="block-seller-wrapper"><a class="upvote-seller" href="#">Upvote</a></span>'
-
-            //downvote button 
-            let downvote = '<span class="block-seller-wrapper"><a class="downvote-seller" href="#">Downvote</a></span>'
-
-
-            $(position).after(downvote).after(upvote).after(hide);
-        },
-
-        // Button click handler actions
-        hideUser: function (user) {
-            this.usersBlocked.push(user);
-            if (this.reviewList[user]) {
-                this.reviewList[user].hidden = true;
-                // console.log(this.reviewList);
-            }
-            else {
-                this.reviewList[user] = {
-                    score: 0,
-                    hidden: true
+                if (self.reviewList[user].score < 0) {
+                    self.downvoteUserUI(user);
+                }
+                if (self.reviewList[user].hidden) {
+                    self.hideUserUI(user);
                 }
             }
+        });
 
-            let object = {};
-            // object['blocked-sellers'] = this.usersBlocked;
-            object['review-list'] = this.reviewList;
-            chrome.storage.sync.set(object);
-            this.hideUserUI(user);
-        },
 
-        upvoteUser: function (user, reason) {
+        // chrome.storage.sync.get(['blocked-sellers'], function (blockedSellers) {
+        //     let results = blockedSellers['blocked-sellers'] ? blockedSellers['blocked-sellers'] : [];
+        //     self.usersBlocked = results;
 
-            // Add score + reason 
-            if (this.reviewList[user]) {
-                let seller = this.reviewList[user];
-                seller.score += 1;
-                if (seller.reason !== undefined || seller.reason !== null) {
-                    seller.reason.push(reason);
-                }
+        //     _.each(self.usersBlocked, function (seller) {
+        //         self.hideUser(seller);
+        //     });
+        // });
+    },
+
+    // Set-up buttons:
+    allWhispers: function () {
+        let self = this;
+        let sellers = this.isCurrencyPage ? $('.displayoffer-bottom span.right a') : $('.whisper-btn');
+
+        _.each(sellers, function (seller) {
+            self.appendButtons(seller);
+        });
+    },
+    appendButtons: function (position) {
+        // block button
+        let hide = '<span class="block-seller-wrapper"><a class="hide-seller" href="#">Hide User</a></span>'
+
+        // upvote button
+        let upvote = '<span class="block-seller-wrapper"><a class="upvote-seller" href="#">Upvote</a></span>'
+
+        //downvote button 
+        let downvote = '<span class="block-seller-wrapper"><a class="downvote-seller" href="#">Downvote</a></span>'
+
+
+        $(position).after(downvote).after(upvote).after(hide);
+    },
+
+    // Button click handler actions
+    hideUser: function (user) {
+        this.usersBlocked.push(user);
+        if (this.reviewList[user]) {
+            this.reviewList[user].hidden = true;
+            // console.log(this.reviewList);
+        }
+        else {
+            this.reviewList[user] = {
+                score: 0,
+                hidden: true
             }
-            // If user didn't exist yet, add them
-            else {
-                this.reviewList[user] = {
-                    score: 1,
-                    hidden: false
-                }
-                let seller = this.reviewList[user];
-                seller.reason = [];
+        }
+
+        let object = {};
+        // object['blocked-sellers'] = this.usersBlocked;
+        object['review-list'] = this.reviewList;
+        chrome.storage.sync.set(object);
+        this.hideUserUI(user);
+    },
+
+    upvoteUser: function (user, reason) {
+
+        // Add score + reason 
+        if (this.reviewList[user]) {
+            let seller = this.reviewList[user];
+            seller.score += 1;
+            if (seller.reason !== undefined || seller.reason !== null) {
                 seller.reason.push(reason);
             }
-            if (this.reviewList[user].score > 0) {
-                this.upvoteUserUI(user);
+        }
+        // If user didn't exist yet, add them
+        else {
+            this.reviewList[user] = {
+                score: 1,
+                hidden: false
             }
-            // Chrome extension puts in the value. Create an object to properly format
-            let object = {};
-            object['review-list'] = this.reviewList;
-            chrome.storage.sync.set(object);
-        },
+            let seller = this.reviewList[user];
+            seller.reason = [];
+            seller.reason.push(reason);
+        }
+        if (this.reviewList[user].score > 0) {
+            this.upvoteUserUI(user);
+        }
+        // Chrome extension puts in the value. Create an object to properly format
+        let object = {};
+        object['review-list'] = this.reviewList;
+        chrome.storage.sync.set(object);
+    },
 
-        downvoteUser: function (user, reason) {
+    downvoteUser: function (user, reason) {
 
-            // Add score + reason 
-            if (this.reviewList[user]) {
-                let seller = this.reviewList[user];
-                seller.score -= 1;
-                if (seller.reason !== undefined || seller.reason !== null) {
-                    seller.reason.push(reason);
-                }
-            }
-            // If user didn't exist yet, add them
-            else {
-                this.reviewList[user] = {
-                    score: -1,
-                    hidden: false
-                }
-                let seller = this.reviewList[user];
-                seller.reason = [];
+        // Add score + reason 
+        if (this.reviewList[user]) {
+            let seller = this.reviewList[user];
+            seller.score -= 1;
+            if (seller.reason !== undefined || seller.reason !== null) {
                 seller.reason.push(reason);
             }
-            if (this.reviewList[user].score < 0) {
-                this.downvoteUserUI(user);
+        }
+        // If user didn't exist yet, add them
+        else {
+            this.reviewList[user] = {
+                score: -1,
+                hidden: false
             }
-            // Chrome extension puts in the value. Create an object to properly format
-            let object = {};
-            object['review-list'] = this.reviewList;
-            chrome.storage.sync.set(object);
-        },
+            let seller = this.reviewList[user];
+            seller.reason = [];
+            seller.reason.push(reason);
+        }
+        if (this.reviewList[user].score < 0) {
+            this.downvoteUserUI(user);
+        }
+        // Chrome extension puts in the value. Create an object to properly format
+        let object = {};
+        object['review-list'] = this.reviewList;
+        chrome.storage.sync.set(object);
+    },
 
-        // UI methods
-        hideUserUI: function (user) {
-            if (this.isCurrencyPage) {
-                $('.displayoffer[data-ign="' + user + '"]').hide()
-            } else {
-                $('.search-results tbody[data-ign="' + user + '"]').hide()
-            }
-        },
-        upvoteUserUI: function (user) {
-            // console.log("we made it here bud!");
-            if (this.isCurrencyPage) {
-                $('.displayoffer[data-ign="' + user + '"]').children().css('background-color', 'green');
-            } else {
-                $('.search-results tbody[data-ign="' + user + '"]').children().css('background-color', 'green');
-            }
-        },
-        downvoteUserUI: function (user) {
-            if (this.isCurrencyPage) {
-                $('.displayoffer[data-ign="' + user + '"]').children().css('background-color', 'red');
-            } else {
-                $('.search-results tbody[data-ign="' + user + '"]').children().css('background-color', '#8D021F');
-            }
+    // UI methods
+    hideUserUI: function (user) {
+        if (this.isCurrencyPage) {
+            $('.displayoffer[data-ign="' + user + '"]').hide()
+        } else {
+            $('.search-results tbody[data-ign="' + user + '"]').hide()
+        }
+    },
+    upvoteUserUI: function (user) {
+        // console.log("we made it here bud!");
+        if (this.isCurrencyPage) {
+            $('.displayoffer[data-ign="' + user + '"]').children().css('background-color', 'green');
+        } else {
+            $('.search-results tbody[data-ign="' + user + '"]').children().css('background-color', 'green');
+        }
+    },
+    downvoteUserUI: function (user) {
+        if (this.isCurrencyPage) {
+            $('.displayoffer[data-ign="' + user + '"]').children().css('background-color', 'red');
+        } else {
+            $('.search-results tbody[data-ign="' + user + '"]').children().css('background-color', '#8D021F');
         }
     }
+}
 
-    // Hide user click handler
-    $(document).on('click', '.hide-seller', function (e) {
-        e.preventDefault();
-        let ign = Blocker.isCurrencyPage ? $(this).parents('.displayoffer').data('ign') : $(this).parents('tbody').data('ign');
+// Hide user click handler
+$(document).on('click', '.hide-seller', function (e) {
+    e.preventDefault();
+    let ign = Blocker.isCurrencyPage ? $(this).parents('.displayoffer').data('ign') : $(this).parents('tbody').data('ign');
 
-        if (ign) {
-            if (confirm('Block this seller?')) {
-                Blocker.hideUser(ign);
-            }
+    if (ign) {
+        if (confirm('Block this seller?')) {
+            Blocker.hideUser(ign);
         }
-    });
+    }
+});
 
-    // Upvote user click handler
-    $(document).on('click', '.upvote-seller', function (e) {
-        e.preventDefault();
-        let ign = Blocker.isCurrencyPage ? $(this).parents('.displayoffer').data('ign') : $(this).parents('tbody').data('ign');
 
-        if (ign) {
-            let reason = prompt('Want to leave a reason why you enjoyed trading with this user?')
-            Blocker.upvoteUser(ign, reason);
-        }
-    });
+// Upvote user click handler
+$(document).on('click', '.upvote-seller', function (e) {
+    e.preventDefault();
+    let ign = Blocker.isCurrencyPage ? $(this).parents('.displayoffer').data('ign') : $(this).parents('tbody').data('ign');
 
-    // downvote user click handler
-    $(document).on('click', '.downvote-seller', function (e) {
-        e.preventDefault();
-        let ign = Blocker.isCurrencyPage ? $(this).parents('.displayoffer').data('ign') : $(this).parents('tbody').data('ign');
+    if (ign) {
+        let reason = prompt('Want to leave a reason why you enjoyed trading with this user?')
+        Blocker.upvoteUser(ign, reason);
+    }
+});
 
-        if (ign) {
-            let reason = prompt("What'd they do wrong this time");
-            Blocker.downvoteUser(ign, reason);
-        }
-    });
 
-    $(document).ready(function () {
-        Blocker.init()
-    });
+// downvote user click handler
+$(document).on('click', '.downvote-seller', function (e) {
+    e.preventDefault();
+    let ign = Blocker.isCurrencyPage ? $(this).parents('.displayoffer').data('ign') : $(this).parents('tbody').data('ign');
 
-})(window)
+    if (ign) {
+        let reason = prompt("What'd they do wrong this time");
+        Blocker.downvoteUser(ign, reason);
+    }
+});
+
+$(document).ready(function () {
+    Blocker.init()
+});
+
+// })(window)
--- js/popup.js
+++ js/popup.js
@@ -3,157 +3,164 @@
 
 // TODO: Turn this into an actual class.
 
-(function (window) {
 
-    let BlockerPopUp = {
-        sellersBlocked: [],
-        reviewList: {},
+// (function (window) {
+let BlockerPopUp = {
+    sellersBlocked: [],
+    reviewList: {},
 
-        init: function () {
-            this.buildBlockedSellersList()
-        },
-        buildBlockedSellersList: function () {
-            let blockedSellerList = $('.blocked-seller-list');
-            let self = this;
+    init: function () {
+        this.buildBlockedSellersList()
+    },
+    buildBlockedSellersList: function () {
+        let blockedSellerList = $('.blocked-seller-list');
+        let self = this;
 
-            chrome.storage.sync.get(['review-list'], function (reviewList) {
-                let results = reviewList['review-list'] ? reviewList['review-list'] : {};
-                console.log("Popup storage succesfully receieved: ", results);
-                self.reviewList = results;
+        chrome.storage.sync.get(['review-list'], function (reviewList) {
+            let results = reviewList['review-list'] ? reviewList['review-list'] : {};
+            console.log("Popup storage succesfully receieved: ", results);
+            self.reviewList = results;
 
-                for (let seller in self.reviewList) {
-                    let sellerNumber = 0;
-
-                    // Create table row for seller
-                    let item = `
+            for (let seller in self.reviewList) {
+                let sellerNumber = 0;
+                $('.no-items').hide();
+                // Create table row for seller
+                let item = `
                     <tr>
                     <th scope="row">${sellerNumber}</th>
                     <td>${self.reviewList[seller]['score']}</td>
                     <td>${seller}</td>`
-                    //Append all reasons as new columns to that seller
-                    for (let reason in self.reviewList[seller]['reason']) {
-                        // if it's the first reason, put it on the same row
-                        if (reason == 0) {
-                            console.log('sup my guy');
-                            item += `
+                //Append all reasons as new columns to that seller
+                for (let reason in self.reviewList[seller]['reason']) {
+                    // if it's the first reason, put it on the same row
+                    if (reason == 0) {
+                        item += `
                         <td> ${self.reviewList[seller]['reason'][reason]} </td>
                         `
-                        }
-                        // if it's not, 
-                        else {
-                            item += `
+                    }
+                    // if it's not, 
+                    else {
+                        item += `
                             <tr>
                             <td></td>
                             <td></td>
                             <td></td>
                             <td> ${self.reviewList[seller]['reason'][reason]} </td>
                             </tr>`
-                        }
                     }
-
-                    // Close the table row block
-                    item += `
-                    </tr>`
-                    // Append to table
-                    blockedSellerList.append(item);
-                    sellerNumber++;
                 }
-            });
 
-            // Legacy code
-            // chrome.storage.sync.get(['blocked-sellers'], function (blockedSellers) {
-            //     let results = blockedSellers['blocked-sellers'] ? blockedSellers['blocked-sellers'] : [];
-            //     self.sellersBlocked = results;
-
-            //     $('.no-items').show();
-            //     if (self.sellersBlocked.length) {
-            //         $('.no-items').hide();
-            //     }
-
-            //     _.each(self.sellersBlocked, function (seller) {
-            //         let item = "<li data-ign='" + seller + "'><span class='left'>" + seller + "</span><span class='right'><a href='#' class='remove-seller-from-blacklist'>Remove</a></span></li>";
-
-            //         blockedSellerList.append(item);
-            //     });
-            // });
-        },
-        removeSellerFromList: function (sellerToRemove) {
-            this.sellersBlocked = _.filter(this.sellersBlocked, function (seller) { return seller != sellerToRemove; });
-            let jsonObj = {};
-            jsonObj['blocked-sellers'] = this.sellersBlocked;
-            chrome.storage.sync.set(jsonObj, function (obj) {
-                $('.blocked-seller-list').empty();
-                BlockerPopUp.buildBlockedSellersList();
-            });
-        },
-        massAddUsersToBlacklist: function (text) {
-            let igns = text.split("\n");
-            for (let i = 0; i < igns.length; i++) {
-                let ign = igns[i].trim();
-                this.sellersBlocked.push(ign);
+                // Close the table row block
+                item += `
+                    </tr>`
+                // Append to table
+                blockedSellerList.append(item);
+                sellerNumber++;
             }
-            this.sellersBlocked = _.uniq(this.sellersBlocked);
-            let jsonObj = {};
-            jsonObj['blocked-sellers'] = this.sellersBlocked;
-            chrome.storage.sync.set(jsonObj, function (obj) {
-                $('.blocked-seller-list').empty();
-                BlockerPopUp.buildBlockedSellersList();
-                $('.sellers-to-block-textarea').val('');
-            });
-        },
-        removeAllFromBlacklist: function () {
-            this.sellersBlocked = []
-            let jsonObj = {};
-            jsonObj['blocked-sellers'] = this.sellersBlocked;
-            chrome.storage.sync.set(jsonObj, function (obj) {
-                $('.blocked-seller-list').empty();
-                BlockerPopUp.buildBlockedSellersList();
-            });
-        }
-    };
-
-    // Initalize code
-    $(document).ready(function () {
-
-        BlockerPopUp.init();
-
-        // Highlights the row you click on. TODO: Convert from targeting the list to targeting the table.
-        $('ul.tabs li').click(function () {
-            let tab_id = $(this).attr('data-tab');
-
-            $('ul.tabs li').removeClass('active');
-            $('.tab-content').removeClass('active');
-
-            $(this).addClass('active');
-            $("#" + tab_id).addClass('active');
         });
-    });
 
-    // Click Handlers
-    $(document).on('click', '.remove-seller-from-blacklist', function (e) {
-        e.preventDefault();
-        let ign = $(this).parents('li').data('ign');
-        if (confirm('Remove this user from your blacklist?')) {
-            BlockerPopUp.removeSellerFromList(ign);
+        // Legacy code
+        // chrome.storage.sync.get(['blocked-sellers'], function (blockedSellers) {
+        //     let results = blockedSellers['blocked-sellers'] ? blockedSellers['blocked-sellers'] : [];
+        //     self.sellersBlocked = results;
+
+        // $('.no-items').show();
+        // if (self.sellersBlocked.length) {
+        //     $('.no-items').hide();
+        // }
+
+        //     _.each(self.sellersBlocked, function (seller) {
+        //         let item = "<li data-ign='" + seller + "'><span class='left'>" + seller + "</span><span class='right'><a href='#' class='remove-seller-from-blacklist'>Remove</a></span></li>";
+
+        //         blockedSellerList.append(item);
+        //     });
+        // });
+    },
+    removeSellerFromList: function (sellerToRemove) {
+        this.sellersBlocked = _.filter(this.sellersBlocked, function (seller) { return seller != sellerToRemove; });
+        let jsonObj = {};
+        jsonObj['blocked-sellers'] = this.sellersBlocked;
+        chrome.storage.sync.set(jsonObj, function (obj) {
+            $('.blocked-seller-list').empty();
+            BlockerPopUp.buildBlockedSellersList();
+        });
+    },
+    massAddUsersToBlacklist: function (text) {
+        let igns = text.split("\n");
+        for (let i = 0; i < igns.length; i++) {
+            let ign = igns[i].trim();
+            this.sellersBlocked.push(ign);
         }
-        return false;
-    });
+        this.sellersBlocked = _.uniq(this.sellersBlocked);
+        let jsonObj = {};
+        jsonObj['blocked-sellers'] = this.sellersBlocked;
+        chrome.storage.sync.set(jsonObj, function (obj) {
+            $('.blocked-seller-list').empty();
+            BlockerPopUp.buildBlockedSellersList();
+            $('.sellers-to-block-textarea').val('');
+        });
+    },
+    removeAllFromBlacklist: function () {
+        this.reviewList = {};
 
-    $(document).on('click', '.parse-mass-add-sellers', function (e) {
-        e.preventDefault();
-        if (confirm('Add these users to your blacklist?')) {
-            let text = $('.sellers-to-block-textarea').val();
-            BlockerPopUp.massAddUsersToBlacklist(text);
-        }
-        return false;
-    });
+        let object = {};
+        object['review-list'] = this.reviewList;
+        chrome.storage.sync.set(object, function (obj) {
+            $('.blocked-seller-list tr').remove();
+            BlockerPopUp.buildBlockedSellersList();
+        })
+        // this.sellersBlocked = []
+        // let jsonObj = {};
+        // jsonObj['blocked-sellers'] = this.sellersBlocked;
+        // chrome.storage.sync.set(jsonObj, function (obj) {
+        //     $('.blocked-seller-list').empty();
+        //     BlockerPopUp.buildBlockedSellersList();
+        // });
+    }
+};
 
-    $(document).on('click', '.clear-all-users', function (e) {
-        e.preventDefault();
-        if (confirm('Remove all users from your blacklist?')) {
-            BlockerPopUp.removeAllFromBlacklist();
-        }
-        return false;
-    });
+// Initalize code
+$(document).ready(function () {
 
-})(window);
+    BlockerPopUp.init();
+
+    // Highlights the row you click on. TODO: Convert from targeting the list to targeting the table.
+    $('ul.tabs li').click(function () {
+        let tab_id = $(this).attr('data-tab');
+
+        $('ul.tabs li').removeClass('active');
+        $('.tab-content').removeClass('active');
+
+        $(this).addClass('active');
+        $("#" + tab_id).addClass('active');
+    });
+});
+
+// Click Handlers
+$(document).on('click', '.remove-seller-from-blacklist', function (e) {
+    e.preventDefault();
+    let ign = $(this).parents('li').data('ign');
+    if (confirm('Remove this user from your blacklist?')) {
+        BlockerPopUp.removeSellerFromList(ign);
+    }
+    return false;
+});
+
+$(document).on('click', '.parse-mass-add-sellers', function (e) {
+    e.preventDefault();
+    if (confirm('Add these users to your blacklist?')) {
+        let text = $('.sellers-to-block-textarea').val();
+        BlockerPopUp.massAddUsersToBlacklist(text);
+    }
+    return false;
+});
+
+$(document).on('click', '.clear-all-users', function (e) {
+    e.preventDefault();
+    if (confirm('Remove all users from your blacklist?')) {
+        BlockerPopUp.removeAllFromBlacklist();
+    }
+    return false;
+});
+
+// })(window);
